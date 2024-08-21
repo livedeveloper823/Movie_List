c0d5f7a43f4a08f90dcd18d3cd895afd48e36629
laihaoqiang <laihaoqiang34@gmail.com>
2024-08-22 06:04:33 +1000

    addvideo funtion implemented

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router"
 import Button from "./Button"
 
-const SubmitForm = () => {
+interface SubmitFormProps {
+    onClick?: () => void
+}
+
+const SubmitForm: React.FC<SubmitFormProps> = ({ onClick }) => {
+    const navigate = useNavigate()
     return (
         <div className="flex gap-5 text-center">
-            <Button text="Cancel" className="sm:px-14 px-10 py-3 font-bold border-2 border-white rounded-[10px]" />
-            <Button text="Submit" className="sm:px-14 px-10 py-3 font-bold bg-primary border-2 border-primary rounded-[10px]" />
+            <Button text="Cancel" className="sm:px-14 px-10 py-3 font-bold border-2 border-white rounded-[10px]" onClick={() => navigate("/main") } />
+            <Button text="Submit" className="sm:px-14 px-10 py-3 font-bold bg-primary border-2 border-primary rounded-[10px]" onClick={onClick} />
         </div>
     )
 }
--- src/pages/movies/Add.tsx
+++ src/pages/movies/Add.tsx
@@ -1,30 +1,82 @@
 import { Download } from "lucide-react";
 import image from '../../assets/image.png';
 import SubmitForm from "../../components/SubmitForm";
+import { VideoProps } from "../../types";
+import { useDispatch } from "../../store";
+import { useState, ChangeEvent } from "react";
+import { addVideosData } from "../../store/reducers/videos";
 
 const AddMovie = () => {
+    const dispatch = useDispatch();
+    const [videoData, setVideoData] = useState<VideoProps>({
+        title: "",
+        image: "",
+        publishingDate: "",
+    });
+
+    console.log("videodata============>", videoData);
+
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const videoTitle = e.target.value;
+        setVideoData({
+            ...videoData,
+            title: videoTitle
+        });
+    };
+    const photoUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const reader = new FileReader();
+        const file = e.target.files?.[0]; // Use optional chaining to handle undefined
+
+        if (file) {
+            reader.onloadend = () => {
+                setVideoData({
+                    ...videoData,
+                    image: reader.result as string
+                });
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleYearChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const publishingYear = e.target.value;
+        setVideoData({
+            ...videoData,
+            publishingDate: publishingYear
+        })
+    }
+
+    const handleSubmit = () => {
+        dispatch(addVideosData(videoData))
+    }
+
     return (
         <>
             <div className="text-white font-montserrat xl:px-80 lg:px-40 md:px-20 px-10 xl:pt-40 pt-20">
                 <p className="md:text-5xl text-3xl font-semibold">Create a new movie</p>
                 <div className="grid mt-20 gap-20 md:grid-cols-1 lg:grid-cols-2">
                     <div className="order-2 md:row-span-2 lg:order-1">
-                        <div className="lg:h-full h-[200px] border-2 border-white border-dashed rounded-md flex justify-center items-center hover:cursor-pointer">
-                            <div className="flex flex-col items-center gap-3">
-                                <Download />
-                                <p className="text-sm">Drop an image here</p>
+                        <label htmlFor="photo-upload" className="fas">
+                            <div className="lg:h-full h-[200px] border-2 border-white border-dashed rounded-md flex justify-center items-center hover:cursor-pointer">
+                                {videoData.image == '' ? <div className="flex flex-col items-center gap-3">
+                                    <Download />
+                                    <p className="text-sm">Drop an image here</p>
+                                </div> :
+                                    <img src={videoData.image} alt="video" className="relative w-auto h-[100%]" />}
                             </div>
-                        </div>
+                            <input id="photo-upload" type="file" className="hidden" onChange={photoUpload} />
+                        </label>
                     </div>
                     <div className="order-1">
                         <div className="flex flex-col gap-4">
-                            <input type="text" placeholder="Title" className="lg:w-96" />
-                            <input type="text" placeholder="Publishing year" className="lg:w-56" />
+                            <input type="text" placeholder="Title" className="lg:w-96" onChange={handleTitleChange} />
+                            <input type="text" placeholder="Publishing year" className="lg:w-56" onChange={handleYearChange} />
                         </div>
                     </div>
                     <div className="order-3">
                         <div className="flex lg:justify-start justify-center">
-                            <SubmitForm />
+                            <SubmitForm onClick={handleSubmit}/>
                         </div>
                     </div>
                 </div>
