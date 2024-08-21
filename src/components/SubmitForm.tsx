9b7cdc486073d76754c464be35b3d1a81ffbf396
laihaoqiang <laihaoqiang34@gmail.com>
2024-08-21 22:02:57 +1000

    frontend done except state management

--- src/components/SubmitForm.tsx
+++ src/components/SubmitForm.tsx
@@ -3,8 +3,8 @@ import Button from "./Button"
 const SubmitForm = () => {
     return (
         <div className="flex gap-5 text-center">
-            <Button text="Cancel" className="px-14 py-3 border-2 border-white rounded-[10px]" />
-            <Button text="Submit" className="px-14 py-3 bg-primary border-2 border-primary rounded-[10px]" />
+            <Button text="Cancel" className="sm:px-14 px-10 py-3 font-bold border-2 border-white rounded-[10px]" />
+            <Button text="Submit" className="sm:px-14 px-10 py-3 font-bold bg-primary border-2 border-primary rounded-[10px]" />
         </div>
     )
 }
--- src/pages/movies/Add.tsx
+++ src/pages/movies/Add.tsx
@@ -5,23 +5,27 @@ import SubmitForm from "../../components/SubmitForm";
 const AddMovie = () => {
     return (
         <>
-            <div className="text-white font-montserrat px-40 pt-20">
-                <p className="text-5xl font-semibold">Create a new Movie</p>
-                <div className="flex gap-40 mt-20">
-                    <button>
-                        <div className="w-[450px] h-[500px] border-2 border-white border-dashed rounded-md flex justify-center items-center">
+            <div className="text-white font-montserrat xl:px-80 lg:px-40 md:px-20 px-10 xl:pt-40 pt-20">
+                <p className="md:text-5xl text-3xl font-semibold">Create a new movie</p>
+                <div className="grid mt-20 gap-20 md:grid-cols-1 lg:grid-cols-2">
+                    <div className="order-2 md:row-span-2 lg:order-1">
+                        <div className="lg:h-full h-[200px] border-2 border-white border-dashed rounded-md flex justify-center items-center hover:cursor-pointer">
                             <div className="flex flex-col items-center gap-3">
                                 <Download />
                                 <p className="text-sm">Drop an image here</p>
                             </div>
                         </div>
-                    </button>
-                    <div className="flex flex-col gap-12">
+                    </div>
+                    <div className="order-1">
                         <div className="flex flex-col gap-4">
-                            <input type="text" placeholder="Title" className="w-96" />
-                            <input type="text" placeholder="Publishing year" className="w-56" />
+                            <input type="text" placeholder="Title" className="lg:w-96" />
+                            <input type="text" placeholder="Publishing year" className="lg:w-56" />
                         </div>
-                        <SubmitForm />
+                    </div>
+                    <div className="order-3">
+                        <div className="flex lg:justify-start justify-center">
+                            <SubmitForm />
+                        </div>
                     </div>
                 </div>
             </div>
