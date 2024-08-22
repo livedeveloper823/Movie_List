0a71e184418da4d66632a3d5ef9dc192f79f4218
laihaoqiang <laihaoqiang34@gmail.com>
2024-08-22 21:21:39 +1000

    frontend completed

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 interface AuthContextType {
